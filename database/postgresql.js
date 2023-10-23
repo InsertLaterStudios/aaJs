ce6444641e83beb3c2f7b117d820567fee93f1ab
omato2468 <73813683+omato2468@users.noreply.github.com>
2023-10-22 23:14:17 -0400

    Logging postgresql.js

--- database/postgresql.js
+++ database/postgresql.js
@@ -5,23 +5,22 @@
 
 
 // // ENVIROMENT // //
-const DATABASE = {
+if (!process.env.PG_HOST) console.log("ERROR aaJs/database/postgresql.js DATABASE.host")
+if (!process.env.PG_PORT) console.log("ERROR aaJs/database/postgresql.js DATABASE.port")
+if (!process.env.PG_USER) console.log("ERROR aaJs/database/postgresql.js DATABASE.user")
+if (!process.env.PG_DATABASE) console.log("ERROR aaJs/database/postgresql.js DATABASE.database")
+if (!process.env.PG_PASSWORD) console.log("ERROR aaJs/database/postgresql.js DATABASE.password")
+
+console.log()
+
+const { Pool } = require("pg")
+const pool = new Pool({
 	host: process.env.PG_HOST,
 	port: process.env.PG_PORT,
 	user: process.env.PG_USER,
 	database: process.env.PG_DATABASE,
 	password: process.env.PG_PASSWORD,
-}
-if (!DATABASE.host) console.log("aaJs/database/postgresql.js DATABASE.host")
-if (!DATABASE.port) console.log("aaJs/database/postgresql.js DATABASE.port")
-if (!DATABASE.user) console.log("aaJs/database/postgresql.js DATABASE.user")
-if (!DATABASE.database) console.log("aaJs/database/postgresql.js DATABASE.database")
-if (!DATABASE.password) console.log("aaJs/database/postgresql.js DATABASE.password")
-
-
-
-const { Pool } = require("pg")
-const pool = new Pool(DATABASE)
+})
 pool.on("error", (err)=>{
 	console.error("ERROR aaJs/database/postgresql.js pool.on error")
 	console.error(err)
